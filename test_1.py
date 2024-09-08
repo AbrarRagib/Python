430bd25de938c6989d67f38158df3f3c78d98450
Abrar Ragib <arsragib@gmail.com>
2024-09-08 15:28:19 +0600

    Dictionary

--- test_1.py
+++ test_1.py
@@ -1004,24 +1004,91 @@ Comments, Escape Sequences & Print Statement
 # git push origin main
 
 
-from datetime import datetime
 
-# Function to calculate age
-def calculate_age(birthdate):
-    today = datetime.today()
-    age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
-    return age
 
-# Input the birthdate in YYYY-MM-DD format
-birthdate_str = input("Enter your birthdate (YYYY-MM-DD): ")
 
-# Convert the string input to a date object
-birthdate = datetime.strptime(birthdate_str, "%Y-%m-%d")
 
-# Calculate and print the age
-age = calculate_age(birthdate)
-print(f"Your age is: {age} years")
 
+
+
+
+
+
+
+
+
+
+
+# # Dictionary Methods in Python
+# Dictionary uses several built-in methods for manipulation.They are listed below
+
+
+# update()
+# The update() method updates the value of the key provided to it if the item already exists in the dictionary, else it creates a new key-value pair.
+
+# Example:
+
+# info = {'name':'Karan', 'age':19, 'eligible':True}
+# print(info)
+# info.update({'age':20})
+# info.update({'DOB':2001})
+# print(info)
+
+
+# Removing items from dictionary:
+# There are a few methods that we can use to remove items from dictionary.
+
+# clear():
+# The clear() method removes all the items from the list.
+
+# # Example:
+# info = {'name':'Karan', 'age':19, 'eligible':True}
+# info.clear()
+# print(info)
+
+
+
+# pop():
+# The pop() method removes the key-value pair whose key is passed as a parameter.
+
+# Example:
+
+# info = {'name':'Karan', 'age':19, 'eligible':True}
+# info.pop('eligible')
+# print(info)
+
+
+
+# popitem():
+# The popitem() method removes the last key-value pair from the dictionary.
+
+# Example:
+
+# info = {'name':'Karan', 'age':19, 'eligible':True, 'DOB':2003}
+# info.popitem()
+# print(info)
+
+
+
+# del:
+# we can also use the del keyword to remove a dictionary item.
+
+# Example:
+
+# info = {'name':'Karan', 'age':19, 'eligible':True, 'DOB':2003}
+# print(info)
+# del info['age']
+# print(info)
+
+
+# If key is not provided, then the del keyword will delete the dictionary entirely.
+
+# Example:
+
+# info = {'name':'Karan', 'age':19, 'eligible':True, 'DOB':2003}
+# print(info)
+# del info
+# print(info)
 
 
 
