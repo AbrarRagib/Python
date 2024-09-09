56267ff08358297e3b1d32f86d854158b8cb7d69
Abrar Ragib <arsragib@gmail.com>
2024-09-09 15:13:16 +0600

    For loops with else

--- test_1.py
+++ test_1.py
@@ -1093,3 +1093,40 @@ Comments, Escape Sequences & Print Statement
 
 
 
+
+
+
+
+
+# for Loop with else in Python
+# Python - else in Loop
+# As you have learned before, the else clause is used along with the if statement.
+
+# Python allows the else keyword to be used with the for and while loops too. The else block appears after the body of the loop. The statements in the else block will be executed after all iterations are completed. The program exits the loop only after the else block is executed.
+
+
+# Example:
+
+
+# for x in range(5):
+#     print ("iteration no {} in for loop".format(x+1))
+# else:
+#     print ("else block in loop")
+# print ("Out of loop")
+
+
+# i = 0
+# while i<7:
+#   print(i)
+#   i = i + 1
+#   # if i == 4:
+#   #   break
+
+# else:
+#   print("Sorry no i")
+
+# for x in range(5):
+#     print ("iteration no {} in for loop".format(x+1))
+# else:
+#     print ("else block in loop")
+# print ("Out of loop")
