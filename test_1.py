4d23b160b749ab5bcb8b510999f10a6c3ee259be
Abrar Ragib <arsragib@gmail.com>
2024-09-10 07:08:05 +0600

    Finally clause

--- test_1.py
+++ test_1.py
@@ -1247,3 +1247,49 @@ Comments, Escape Sequences & Print Statement
 
 # x = func1()
 # print(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Raising Custom errors
+# In python, we can raise custom errors by using the raise keyword.
+
+
+
+# salary = int(input("Enter salary amount: "))
+# if not 2000 < salary < 5000:
+#     raise ValueError("Not a valid salary")
+
+
+# Defining Custom Exceptions
+# In Python, we can define custom exceptions by creating a new class that is derived from the built-in Exception class.
+
+# Here's the syntax to define custom exceptions:
+
+# class CustomError(Exception):
+#   # code ...
+#   pass
+
+# try:
+#   # code ...
+
+# except CustomError:
+#   # code...
+
+# This is useful because sometimes we might want to do something when a particular exception is raised. For example, sending an error report to the admin, calling an api, etc.
+
+a = int(input("Enter any value between 5 and 9"))
+
+if(a<5  or a>9):
+  raise  ValueError("Value should be between 5 and 9")
