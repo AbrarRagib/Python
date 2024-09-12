86a8fbd5bb4c57c46e9d9c28626c23b71c859f12
Abrar Ragib <arsragib@gmail.com>
2024-09-12 11:21:11 +0600

    Finally clause

--- test_1.py
+++ test_1.py
@@ -1289,7 +1289,136 @@ Comments, Escape Sequences & Print Statement
 
 # This is useful because sometimes we might want to do something when a particular exception is raised. For example, sending an error report to the admin, calling an api, etc.
 
-a = int(input("Enter any value between 5 and 9"))
+# a = int(input("Enter any value between 5 and 9: "))
 
-if(a<5  or a>9):
-  raise  ValueError("Value should be between 5 and 9")
+# if(a<5  or a>9):
+#   raise  ValueError("Value should be between 5 and 9")
+
+
+
+
+
+# questions = [
+#   [
+#     "Which language was used to create fb?", "Python", "French", "JavaScript",
+#     "Php", "None", 4
+#   ],
+#   [
+#     "Which language was used to create fb?", "Python", "French", "JavaScript",
+#     "Php", "None", 4
+#   ],
+#   [
+#     "Which language was used to create fb?", "Python", "French", "JavaScript",
+#     "Php", "None", 4
+#   ],
+#   [
+#     "Which language was used to create fb?", "Python", "French", "JavaScript",
+#     "Php", "None", 4
+#   ],
+#   [
+#     "Which language was used to create fb?", "Python", "French", "JavaScript",
+#     "Php", "None", 4
+#   ],
+#   [
+#     "Which language was used to create fb?", "Python", "French", "JavaScript",
+#     "Php", "None", 4
+#   ],
+#   [
+#     "Which language was used to create fb?", "Python", "French", "JavaScript",
+#     "Php", "None", 4
+#   ],
+#   [
+#     "Which language was used to create fb?", "Python", "French", "JavaScript",
+#     "Php", "None", 4
+#   ],
+#   [
+#     "Which language was used to create fb?", "Python", "French", "JavaScript",
+#     "Php", "None", 4
+#   ],
+#   [
+#     "Which language was used to create fb?", "Python", "French", "JavaScript",
+#     "Php", "None", 4
+#   ],
+#   [
+#     "Which language was used to create fb?", "Python", "French", "JavaScript",
+#     "Php", "None", 4
+#   ],
+#   [
+#     "Which language was used to create fb?", "Python", "French", "JavaScript",
+#     "Php", "None", 4
+#   ],
+#   [
+#     "Which language was used to create fb?", "Python", "French", "JavaScript",
+#     "Php", "None", 4
+#   ],
+#   [
+#     "Which language was used to create fb?", "Python", "French", "JavaScript",
+#     "Php", "None", 4
+#   ],
+#   [
+#     "Which language was used to create fb?", "Python", "French", "JavaScript",
+#     "Php", "None", 4
+#   ],
+#   [
+#     "Which language was used to create fb?", "Python", "French", "JavaScript",
+#     "Php", "None", 4
+#   ],
+#   [
+#     "Which language was used to create fb?", "Python", "French", "JavaScript",
+#     "Php", "None", 4
+#   ],
+#   [
+#     "Which language was used to create fb?", "Python", "French", "JavaScript",
+#     "Php", "None", 4
+#   ],
+#   [
+#     "Which language was used to create fb?", "Python", "French", "JavaScript",
+#     "Php", "None", 4
+#   ],
+#   [
+#     "Which language was used to create fb?", "Python", "French", "JavaScript",
+#     "Php", "None", 4
+#   ],
+#   [
+#     "Which language was used to create fb?", "Python", "French", "JavaScript",
+#     "Php", "None", 4
+#   ],
+#   [
+#     "Which language was used to create fb?", "Python", "French", "JavaScript",
+#     "Php", "None", 4
+#   ],
+#   [
+#     "Which language was used to create fb?", "Python", "French", "JavaScript",
+#     "Php", "None", 4
+#   ],
+#   [
+#     "Which language was used to create fb?", "Python", "French", "JavaScript",
+#     "Php", "None", 4
+#   ],
+# ]
+
+# levels = [1000, 2000, 3000, 5000, 10000, 20000, 40000, 80000, 160000, 320000]
+# money = 0
+# for i in range(0, len(questions)):
+  
+#   question = questions[i]
+#   print(f"\n\nQuestion for Rs. {levels[i]}")
+#   print(f"a. {question[1]}          b. {question[2]} ")
+#   print(f"c. {question[3]}          d. {question[4]} ")
+#   reply = int(input("Enter your answer (1-4) or  0 to quit:\n" ))
+#   if (reply == 0):
+#     money = levels[i-1]
+#     break
+#   if(reply == question[-1]):
+#     print(f"Correct answer, you have won Rs. {levels[i]}")
+#     if(i == 4):
+#       money = 10000
+#     elif(i == 9):
+#       money = 320000
+#     elif(i == 14):
+#       money = 10000000
+#   else:
+#     print("Wrong answer!")
+#     break 
+
+# print(f"Your take home money is {money}")
