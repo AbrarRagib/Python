91912e32679663dd90b105febe28cb579fa7a111
Abrar Ragib <arsragib@gmail.com>
2024-09-17 11:34:33 +0600

    Import

--- Demo_1.py
+++ Demo_1.py
@@ -1,4 +1,12 @@
-print("Hello WOrld")
+# print("Hello WOrld")
 # git add .
 # git commit -m "Your commit message"
 # git push origin main
+
+def welcome():
+  print("Hello world")
+
+print(__name__)
+
+if __name__ == "__main__":
+  welcome()
