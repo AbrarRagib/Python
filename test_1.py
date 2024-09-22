377e2bf6c55d1284c8bad8f0349083ae15d00d26
Abrar Ragib <arsragib@gmail.com>
2024-09-22 12:51:23 +0600

    Import

--- test_1.py
+++ test_1.py
@@ -2010,7 +2010,7 @@ Comments, Escape Sequences & Print Statement
 
 
 
-x = 1000  # global variable
+x = 10000  # global variable
 
 
 def my_function():
@@ -2022,3 +2022,10 @@ def my_function():
 my_function()
 print(x)  # prints 5
 # print(y) # this will cause an error because y is a local variable and is not accessible outside of the function
+
+
+
+
+
+
+
