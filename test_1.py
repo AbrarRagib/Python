405a3610cdfee8e755f25aaa3839513f5a30c5e2
Abrar Ragib <arsragib@gmail.com>
2024-09-29 11:50:27 +0600

    first

--- test_1.py
+++ test_1.py
@@ -2115,6 +2115,75 @@ Comments, Escape Sequences & Print Statement
 # f.close()
 
 
-# auto open and close when using with
-with open('text.txt', 'a') as f:
-  f.write("Hey I am inside with")
+# # auto open and close when using with
+# with open('text.txt', 'a') as f:
+#   f.write("Hey I am inside with")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# readlines() method
+# The readline() method reads a single line from the file. If we want to read multiple lines, we can use a loop.
+
+# f = open('myfile.txt', 'r')
+# while True:
+#     line = f.readline()
+#     if not line:
+#         break
+#     print(line)
+# The readlines() method reads all the lines of the file and returns them as a list of strings.
+
+# writelines() method
+# The writelines() method in Python writes a sequence of strings to a file. The sequence can be any iterable object, such as a list or a tuple.
+
+# Here's an example of how to use the writelines() method:
+
+# f = open('myfile.txt', 'w')
+# lines = ['line 1\n', 'line 2\n', 'line 3\n']
+# f.writelines(lines)
+# f.close()
+# This will write the strings in the lines list to the file myfile.txt. The \n characters are used to add newline characters to the end of each string.
+
+# Keep in mind that the writelines() method does not add newline characters between the strings in the sequence. If you want to add newlines between the strings, you can use a loop to write each string separately:
+
+# f = open('myfile.txt', 'w')
+# lines = ['line 1', 'line 2', 'line 3']
+# for line in lines:
+#     f.write(line + '\n')
+# f.close()
+# It is also a good practice to close the file after you are done with it.
+
+
+
+
+
+# f = open('text.txt', 'r')
+# i = 0
+# while True:
+#   i = i + 1
+#   line = f.readline()
+#   if not line:
+#     break
+#   m1 = int(line.split(",")[0])
+#   m2 = int(line.split(",")[1])
+#   m3 = int(line.split(",")[2])
+#   print(f"Marks of student {i} in Maths is: {m1*2}")
+#   print(f"Marks of student {i} in English is: {m2*2}")
+#   print(f"Marks of student {i} in SST is: {m3*2}")
+
+#   print(line)
+
+# f = open('text.txt', 'w')
+# lines = ['line 1\n', 'line 2\n', 'line 3\n']
+# f.writelines(lines)
+# f.close()
