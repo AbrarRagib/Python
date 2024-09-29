0ffccb4e4871a00b0698b9cb1aa51dede4ca4bba
Abrar Ragib <arsragib@gmail.com>
2024-09-29 12:40:50 +0600

    lambda

--- test_1.py
+++ test_1.py
@@ -2230,9 +2230,67 @@ Comments, Escape Sequences & Print Statement
 #   print(f.read())
 
 
-with open('text.txt', 'w') as f:
-  f.write('Hello World3!')
-  f.truncate(3)
+# with open('text.txt', 'w') as f:
+#   f.write('Hello World3!')
+#   f.truncate(3)
 
-with open('text.txt', 'r') as f:
-  print(f.read())
+# with open('text.txt', 'r') as f:
+#   print(f.read())
+
+
+
+
+
+
+
+
+
+
+# Lambda Functions in Python
+# In Python, a lambda function is a small anonymous function without a name. It is defined using the lambda keyword and has the following syntax:
+
+# lambda arguments: expression
+# Lambda functions are often used in situations where a small function is required for a short period of time. They are commonly used as arguments to higher-order functions, such as map, filter, and reduce.
+
+# Here is an example of how to use a lambda function:
+
+# # Function to double the input
+# def double(x):
+#   return x * 2
+# # Lambda function to double the input
+# lambda x: x * 2
+# The above lambda function has the same functionality as the double function defined earlier. However, the lambda function is anonymous, as it does not have a name.
+
+# Lambda functions can have multiple arguments, just like regular functions. Here is an example of a lambda function with multiple arguments:
+
+# # Function to calculate the product of two numbers
+# def multiply(x, y):
+#     return x * y
+# # Lambda function to calculate the product of two numbers
+# lambda x, y: x * y
+# Lambda functions can also include multiple statements, but they are limited to a single expression. For example:
+
+# # Lambda function to calculate the product of two numbers,
+# # with additional print statement
+# lambda x, y: print(f'{x} * {y} = {x * y}')
+# In the above example, the lambda function includes a print statement, but it is still limited to a single expression.
+
+# Lambda functions are often used in conjunction with higher-order functions, such as map, filter, and reduce which we will look into later.
+
+
+
+
+# def double(x):
+#   return x*2
+
+def appl(fx, value):
+  return 6 + fx(value)
+
+double = lambda x: x * 2
+cube = lambda x: x * x * x
+avg = lambda x, y, z: (x + y + z) / 3
+
+print(double(5))
+print(cube(5))
+print(avg(3, 5, 10))
+print(appl(lambda x: x * x , 2))
