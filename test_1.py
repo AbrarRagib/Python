bd56c8c5b182f4573644f327648edcbb70e7e49d
Abrar Ragib <arsragib@gmail.com>
2024-09-29 13:27:46 +0600

    ==

--- test_1.py
+++ test_1.py
@@ -2375,3 +2375,79 @@ Comments, Escape Sequences & Print Statement
 # In the above example, the reduce function applies the lambda function lambda x, y: x + y to the elements in the numbers list. The lambda function adds the two arguments x and y and returns the result. The reduce function applies the lambda function to the first two elements in the list (1 and 2), then applies the function to the result (3) and the next element (3), and so on. The final result is the sum of all the elements in the list, which is 15.
 
 # It is important to note that the reduce function requires the functools module to be imported in order to use it.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 'is' vs '==' in Python
+# In Python, is and == are both comparison operators that can be used to check if two values are equal. However, there are some important differences between the two that you should be aware of.
+
+# The is operator compares the identity of two objects, while the == operator compares the values of the objects. This means that is will only return True if the objects being compared are the exact same object in memory, while == will return True if the objects have the same value.
+
+# For example:
+
+# a = [1, 2, 3]
+# b = [1, 2, 3]
+# print(a == b)  # True
+# print(a is b)  # False
+# In this case, a and b are two separate lists that have the same values, so == returns True. However, a and b are not the same object in memory, so is returns False.
+
+# One important thing to note is that, in Python, strings and integers are immutable, which means that once they are created, their value cannot be changed. This means that, for strings and integers, is and == will always return the same result:
+
+# a = "hello"
+# b = "hello"
+# print(a == b)  # True
+# print(a is b)  # True
+# a = 5
+# b = 5
+# print(a == b)  # True
+# print(a is b)  # True
+# In these cases, a and b are both pointing to the same object in memory, so is and == both return True.
+
+# For mutable objects such as lists and dictionaries, is and == can behave differently. In general, you should use == when you want to compare the values of two objects, and use is when you want to check if two objects are the same object in memory.
+
+# I hope this helps clarify the difference between is and == in Python!
+
+
+
+# a = None
+# b = None
+
+# print(a is b) # exact location of object in memory
+# print(a is None) # exact location of object in memory
+# print(a == b) # value
+
+
+# a = [1,2,3]   # immutable so python create it only once
+# b = [1,2,3]
+
+# print(a is b) # exact location of object in memory
+# print(a == b) # value
+
+a = 3  # immutable so python create it only once
+b = 3
+
+print(a is b) # exact location of object in memory
+print(a == b) # value
