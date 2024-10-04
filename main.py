e71dc53431d5544e97f291d224b98fceba3eb168
Abrar Ragib <arsragib@gmail.com>
2024-10-04 08:08:27 +0600

    Decorators

--- main.py
+++ main.py
@@ -2770,22 +2770,25 @@ Comments, Escape Sequences & Print Statement
 
 
 
-class MyClass:
-  def __init__(self, value):
-      self._value = value
+# class MyClass:
+#   def __init__(self, value):
+#       self._value = value
     
-  def show(self):
-    print(f"Value is {self._value}")
+#   def show(self):
+#     print(f"Value is {self._value}")
     
-  @property
-  def ten_value(self):
-      return 10* self._value
+#   @property
+#   def ten_value(self):
+#       return 10* self._value
     
-  @ten_value.setter
-  def ten_value(self, new_value):
-      self._value = new_value/10
+#   @ten_value.setter
+#   def ten_value(self, new_value):
+#       self._value = new_value/10
 
-obj = MyClass(10)
-obj.ten_value = 67
-print(obj.ten_value)
-obj.show()
+# obj = MyClass(10)
+# obj.ten_value = 67
+# print(obj.ten_value)
+# obj.show()
+
+
+
