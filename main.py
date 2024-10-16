7092936cf1ca88c3e96fa139d8224e5926cd0397
Abrar Ragib <arsragib@gmail.com>
2024-10-16 22:48:31 +0600

    static

--- main.py
+++ main.py
@@ -3027,6 +3027,10 @@ Comments, Escape Sequences & Print Statement
 
 
 
+
+
+
+
 # Access Specifiers/Modifiers
 # Access specifiers or access modifiers in python programming are used to limit the access of class variables and class methods outside of class while implementing the concepts of inheritance.
 
@@ -3158,3 +3162,53 @@ Comments, Escape Sequences & Print Statement
 # print(obj1._name)    
 # print(obj1._funName())
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Static methods in Python are methods that belong to a class rather than an instance of the class. They are defined using the @staticmethod decorator and do not have access to the instance of the class (i.e. self). They are called on the class itself, not on an instance of the class. Static methods are often used to create utility functions that don't need access to instance data.
+
+# class Math:
+#     @staticmethod
+#     def add(a, b):
+#         return a + b
+# result = Math.add(1, 2)
+# print(result) # Output: 3
+# In this example, the add method is a static method of the Math class. It takes two parameters a and b and returns their sum. The method can be called on the class itself, without the need to create an instance of the class.
+
+
+# class Math:
+#   def __init__(self, num):
+#     self.num = num
+
+#   def addtonum(self, n):
+#     self.num = self.num +n
+    
+#   @staticmethod
+#   def add(a, b):
+#       return a + b
+
+# # result = Math.add(1, 2)
+# # print(result) # Output: 3
+# a = Math(5)
+# print(a.num)
+# a.addtonum(6)
+# print(a.num)
+
+# print(Math.add(7, 2)) 
