86b60c7b33b6b5b550903a5d6e7c6cab74e4d539
Abrar Ragib <arsragib@gmail.com>
2024-10-26 01:14:51 +0600

    FizzBuzzz

--- basic.py
+++ basic.py
@@ -99,13 +99,40 @@
 
 
 
-def is_palindrome(s):
-    s = s.replace(" ","").lower()
-    return s == s[::-1]
+# def is_palindrome(s):
+#     s = s.replace(" ","").lower()
+#     return s == s[::-1]
 
 
 
-print('Output is: ',is_palindrome("A man a plan a canal Panama"))
+# print('Output is: ',is_palindrome("A man a plan a canal Panama"))
+
+
+
+
+
+# 3. FizzBuzz Problem
+# Problem: Print numbers from 1 to 100. For multiples of 3, print "Fizz" instead of the number. For multiples of 5, print "Buzz." For numbers that are multiples of both 3 and 5, print "FizzBuzz."
+
+
+def fizz_buzz():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+# Example usage:
+fizz_buzz()
+
+
+
+
+
 
 
 
