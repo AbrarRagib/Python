1f600d7765f2e43cc711b3df86258383f7cc423e
Abrar Ragib <arsragib@gmail.com>
2024-10-25 21:01:54 +0600

    largest and smallet number in arry/list

--- basic.py
+++ basic.py
@@ -1,9 +1,34 @@
 # 1. Find the Largest Number in an Array
 # Problem: Write a function to find the largest number in a given array.
 
-def find_largest(arr):
-    if len(arr) == 0:
-        return None
-    largest = arr[0]
-    for num in arr:
-        if num
+# def find_largest(arr):
+#     if len(arr) == 0:
+#         return None
+#     largest = arr[0]
+#     for num in arr:
+#         if num > largest:
+#             largest = num
+#     return largest
+    
+# arr = [3,5,4,6,8,9]
+# print(find_largest(arr))
+
+# #smallest number in array
+
+# def find_smallest(arr):
+#     if len(arr) == 0:
+#         return None
+#     smallest = arr[0]
+#     for num in arr:
+#         if num < smallest:
+#             smallest = num
+#     return smallest
+
+
+# # Example usage:
+# arr = [3, 5, 4, 6, 8, 9]
+# print(find_smallest(arr))  # Output: 3
+
+
+
+
